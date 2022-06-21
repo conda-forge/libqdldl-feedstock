6c0d35b81e0d91afddfbab083c60d40b5070e43f
Silvio Traversaro <silvio@traversaro.it>
2022-06-21 09:46:59 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,10 @@ cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
       -DBUILD_SHARED_LIBS=ON \
+      -DQDLDL_BUILD_SHARED_LIB=ON \
+      -DQDLDL_BUILD_STATIC_LIB=OFF \
       -DBUILD_TESTING=ON \
-      -DUNITTESTS=ON \
+      -DQDLDL_UNITTESTS=ON \
       ..
 
 cmake --build . --config Release --parallel ${CPU_COUNT}
